ac63d68256d07012a6b55ec4a0e0df3a6b52d9f7
userno1 <wplover90@gmail.com>
2022-06-27 12:28:37 +0900

    third commit in experiment1 branch, defined new widget,"NewInfoWidget"

---
+++ lib/new_info_widget.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class NewInfoWidget extends StatelessWidget {
+  NewInfoWidget({this.info = '',});
+  final String info;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color:Colors.indigoAccent[100],
+      child:Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(info),
+      ),
+    );
+  }
+}
+
+
+
